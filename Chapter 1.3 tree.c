98a2600f99f8b51bcac2a3822c52f5d7a02d4b14
G_EEE3 <c1as2icblu3@gmail.com>
2021-02-18 17:00:31 +0900

    ADD Chapter 1.3

--- Chapter 1.3 tree.c
+++ Chapter 1.3 tree.c
@@ -1,8 +1,6 @@
 /*
 by GEEE3, February 16, 2021
-1, 2 ... n의 합을 구하는 프로그램
-사전 판단 반복문인 while문과 for문은 처음에 제어식을 평가한 결과가 False이면 본문은 실행되지 않는다
-이와 달리 사후 판단 반복문인 do문은 루프 본문이 반드시 한 번은 실행된다
+tree의 높이 height를 입력받아 tree를 출력하는 프로그램
 */
 
 #include <stdio.h>
@@ -12,6 +10,7 @@ int main (void) {
     printf("height of TREE : ");
     scanf("%d", &height);
     printf("\n");
+
     for (i = 0; i < height; i++) {
         for (j = 0; j < height - i; j++) {
             printf(" ");
