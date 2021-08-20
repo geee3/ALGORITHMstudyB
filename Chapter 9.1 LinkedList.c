036ee55214a2c7a15516e7e1559dbbdc4ef95c94
GEEE3 <c1as2icblu3@gmail.com>
2021-08-20 20:46:18 +0900

    ADD Chapter 9.1

--- Chapter 9.1 LinkedList.c
+++ Chapter 9.1 LinkedList.c
@@ -21,19 +21,60 @@ So we cannot do binary search with linked list efficiently
 #include <stdlib.h>
 
 struct Node {
-    int data;
+    char data;
     struct Node* next;
 };
+
+void addNode(struct Node* head, char newData) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = newData;
+    newNode->next = NULL;
+    struct Node *last = head;
+
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    
+    while(last->next != NULL)
+        last = last->next;
+    last->next = newNode;
+}
+
+void deleteNode(struct Node** head, char key) {
+    struct Node *tempNode = *head;
+    struct Node *prevNode = NULL;
+
+    if(tempNode != NULL && tempNode->data == key) {
+        *head = tempNode->next;
+        free(tempNode);
+        return;
+    }
+
+    while(tempNode != NULL && tempNode -> data != key) {
+        prevNode = tempNode;
+        tempNode = tempNode->next;
+    }
+
+    if(tempNode == NULL) {
+        printf("\n");
+        printf("ERROR: No Matching Data\n");
+        return;
+    }
+    prevNode->next = tempNode->next;
+    free(tempNode);
+}
 
 void printList(struct Node* head) {
     struct Node* node = head;
     while(1) {
         printf("+---+\n");
-        printf("| %d |\n", node->data);
+        printf("| %c |\n", node->data);
         printf("+---+\n");
+        printf("\n");
+
         if(node->next == NULL)
             break;
-        printf("\n");
         printf("  |\n");
         printf("  V\n");
         printf("\n");
@@ -42,6 +83,8 @@ void printList(struct Node* head) {
 }
 
 int main(void) {
+    int command, endFlag = 0;
+    char userInput;
     struct Node* head = NULL;
     struct Node* second = NULL;
     struct Node* third = NULL;
@@ -50,15 +93,48 @@ int main(void) {
     second = (struct Node*)malloc(sizeof(struct Node));
     third = (struct Node*)malloc(sizeof(struct Node));
 
-    head->data = 1;
+    head->data = 'a';
     head->next = second;
-
-    second->data = 2;
+    second->data = 'b';
     second->next = third;
-
-    third->data = 3;
+    third->data = 'c';
     third->next = NULL;
 
     printList(head);
+
+    while(endFlag == 0) {
+        printf("[Please Enter Number to Execute]\n");
+        printf("1. Add Data.\n");
+        printf("2. Delete Data.\n");
+        printf("3. Print List.\n");
+        printf("4. Terminate\n");
+        scanf("%d", &command);
+        getchar();
+        printf("\n");
+
+        switch(command) {
+            case 1:
+                printf("Please Enter Alphabet to Add: ");
+                scanf("%c", &userInput);
+                getchar();
+                addNode(head, userInput);
+                printf("\n");
+                break;
+            case 2:
+                printf("Please Enter Alphabet to Delete: ");
+                scanf("%c", &userInput);
+                getchar();
+                deleteNode(&head, userInput);
+                printf("\n");
+                break;
+            case 3:
+                printList(head);
+                break;
+            case 4:
+                printf("[Program Termination...]");
+                endFlag = 1;
+                break;
+        }
+    }
     return 0;
 }
